3734fc3e1e6908cf64f517f1cb833d12f76f667a
timas <timmylbueno@gmail.com>
2025-11-25 15:24:22 -0300

    implementation of the 'delete product' route

--- api/deleteProduct.php
+++ api/deleteProduct.php
@@ -1,5 +1,30 @@
 <?php
 
+include_once("../utils/error.php");
+include_once("../utils/success.php");
+include_once("../utils/getData.php");
+include_once("../utils/updateData.php");
+
 function deleteProduct() {
-    return  json_encode(["route" => "Delete product"]);
+    if(!isset($_POST["id"])) return error("Parâmetros faltando!");
+    if((int)$_POST["id"] < 0) return error("Esse não é um id válido!");
+    
+    $id = (int)$_POST["id"];
+    
+    $data = getData() ?? [];
+    if(count($data) === 0) return error("Produto não encontrado!");
+
+    $newData = [];
+    foreach($data as $product) {
+        $productId = $product->id;
+
+        if($productId !== $id) {
+            array_push($newData, $product);
+        }
+    }
+
+    if($newData === $data) return error("Produto não encontrado!");
+
+    updateData($newData);
+    return success(200, "Produto removido com sucesso!");
 }
