0897c539c45ebebcbfede75affbaf66852d5d80c
timas <timmylbueno@gmail.com>
2025-11-28 15:56:08 -0300

    correcting a typo

--- utils/updateData.php
+++ utils/updateData.php
@@ -1,11 +1,8 @@
 <?php
 
-$variablesFile = parse_ini_file("../.env");
-$expirationTime = $variablesFile["MINIMUM_COOKIE_EXPIRATION_TIME"];
-
 function updateData($data) {
     $variablesFile = parse_ini_file("../.env");
-    $expirationTime = $variablesFile["MINUMUM_COOKIE_EXPIRATION_TIME"];
+    $expirationTime = $variablesFile["MINIMUM_COOKIE_EXPIRATION_TIME"];
     
     setcookie("data", json_encode($data), time() + (int)$expirationTime, "/");
 }
